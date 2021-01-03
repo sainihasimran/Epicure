0ac8330b63cf5a9dd9e8358860a5f726ddc751ef
Harsimran Saini <harsimran12326@gmail.com>
2021-01-02 22:56:38 -0500

    Show default user

--- app/src/main/java/com/cegep/epicure/HomeFragment.java
+++ app/src/main/java/com/cegep/epicure/HomeFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.cegep.epicure.helper.CategoryUiHandler;
 import com.cegep.epicure.model.Category;
+import com.cegep.epicure.model.User;
 
 public class HomeFragment extends Fragment implements CategoryUiHandler.CategorySelectedListener {
 
@@ -21,6 +23,17 @@ public class HomeFragment extends Fragment implements CategoryUiHandler.Category
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         new CategoryUiHandler(view, this);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // TODO: 02/01/21 Remove hardcoded user
+        User user = new User();
+
+        TextView nameTextView = view.findViewById(R.id.name);
+        nameTextView.setText(getString(R.string.greeting, user.firstName));
     }
 
     @Override
