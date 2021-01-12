75e059756d7f3c794cab4ba7aec370bbb4ed4a33
Harsimran Saini <harsimran12326@gmail.com>
2021-01-12 02:36:13 -0500

    Setup categories on create recipe screen

--- app/src/main/java/com/cegep/epicure/CreateRecipeFragment.java
+++ app/src/main/java/com/cegep/epicure/CreateRecipeFragment.java
@@ -9,15 +9,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import com.cegep.epicure.model.Category;
 import com.google.android.material.chip.Chip;
 import com.zhihu.matisse.Matisse;
 import com.zhihu.matisse.MimeType;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CreateRecipeFragment extends Fragment {
@@ -49,15 +54,33 @@ public class CreateRecipeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.add_image).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.add_image).setOnClickListener(v -> {
+            String permission = REQUIRED_PERMISSIONS[0];
+            if (ContextCompat.checkSelfPermission(requireContext(), permission) == PackageManager.PERMISSION_GRANTED) {
+                pickImage();
+            } else {
+                requestPermissions(REQUIRED_PERMISSIONS, PERMISSION_REQUEST_CODE);
+            }
+        });
+
+        setupCategories(view);
+    }
+
+    private void setupCategories(View view) {
+        final List<String> categories = new ArrayList<>(Category.values().length);
+        for (Category category : Category.values()) {
+            categories.add(category.toString().charAt(0) + category.toString().substring(1).toLowerCase().replace("_", " "));
+        }
+
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_dropdown_item_1line, categories);
+        AutoCompleteTextView recipeCategoryTextView = view.findViewById(R.id.recipe_category);
+        recipeCategoryTextView.setAdapter(categoryAdapter);
+
+        recipeCategoryTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                String permission = REQUIRED_PERMISSIONS[0];
-                if (ContextCompat.checkSelfPermission(requireContext(), permission) == PackageManager.PERMISSION_GRANTED) {
-                    pickImage();
-                } else {
-                    requestPermissions(REQUIRED_PERMISSIONS, PERMISSION_REQUEST_CODE);
-                }
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO: 12/01/21 replace this with actual implementation
+                Toast.makeText(requireContext(), "Category selected: " + categories.get(position), Toast.LENGTH_SHORT).show();
             }
         });
     }
