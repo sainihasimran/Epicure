4ce9ebfe0100b241be09ffd79fca356e853d2777
Harsimran Saini <harsimran12326@gmail.com>
2021-01-24 21:17:18 -0500

    Setup ingredients view

--- app/src/main/java/com/cegep/epicure/CreateRecipeFragment.java
+++ app/src/main/java/com/cegep/epicure/CreateRecipeFragment.java
@@ -2,6 +2,7 @@ package com.cegep.epicure;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -9,8 +10,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
@@ -19,6 +23,7 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import com.cegep.epicure.model.Category;
 import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 import com.zhihu.matisse.Matisse;
 import com.zhihu.matisse.MimeType;
 import java.util.ArrayList;
@@ -64,6 +69,7 @@ public class CreateRecipeFragment extends Fragment {
 
         setupCategories(view);
         setupDuration(view);
+        setupAddIngredientsChip(view);
     }
 
     private void setupCategories(View view) {
@@ -95,6 +101,36 @@ public class CreateRecipeFragment extends Fragment {
         durationsTextView.setOnItemClickListener(
                 (parent, view1, position, id) -> Toast.makeText(requireContext(), "Duration selected: " + durations.get(position), Toast.LENGTH_SHORT)
                         .show());
+    }
+
+    private void setupAddIngredientsChip(View view) {
+        ChipGroup chipGroup = view.findViewById(R.id.ingredients_group);
+        view.findViewById(R.id.add_ingredient_chip).setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+            builder.setTitle(R.string.add_ingredient);
+
+            View view1 = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_text_input_layout, null, false);
+            EditText inputEditText = view1.findViewById(R.id.input_text);
+            builder.setView(view1);
+
+            builder.setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                Chip chip = (Chip) LayoutInflater.from(requireContext()).inflate(R.layout.ingredient_chip, chipGroup, false);
+                chip.setText(inputEditText.getText());
+                chip.setOnCloseIconClickListener(v1 -> chipGroup.removeView(chip));
+
+                int numOfChildren = chipGroup.getChildCount();
+
+                chipGroup.addView(chip, numOfChildren - 1);
+            });
+
+            builder.setNegativeButton(android.R.string.cancel, null);
+            AlertDialog dialog = builder.create();
+            Window window = dialog.getWindow();
+            if (window != null) {
+                window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+            }
+            dialog.show();
+        });
     }
 
     @Override
