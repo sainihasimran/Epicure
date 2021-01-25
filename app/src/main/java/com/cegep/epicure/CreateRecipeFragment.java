98f48bbdd1ecf01ba5cbc7c2242cdaf0d062e592
Harsimran Saini <harsimran12326@gmail.com>
2021-01-24 22:28:29 -0500

    Validate create recipe input

--- app/src/main/java/com/cegep/epicure/CreateRecipeFragment.java
+++ app/src/main/java/com/cegep/epicure/CreateRecipeFragment.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,12 +20,14 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import com.cegep.epicure.list.StepsAdapter;
 import com.cegep.epicure.list.callback.RemoveItemClickListener;
 import com.cegep.epicure.model.Category;
+import com.cegep.epicure.model.Recipe;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 import com.zhihu.matisse.Matisse;
@@ -54,6 +57,12 @@ public class CreateRecipeFragment extends Fragment implements RemoveItemClickLis
 
     private final List<String> preparationSteps = new ArrayList<>();
 
+    private String selectedCategory;
+
+    private String selectedDuration;
+
+    private Recipe recipe;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -80,6 +89,12 @@ public class CreateRecipeFragment extends Fragment implements RemoveItemClickLis
         setupDuration(view);
         setupAddIngredientsChip(view);
         setupStepsList(view);
+
+        view.findViewById(R.id.create_recipe_button).setOnClickListener(v -> {
+            boolean formValid = validateForm();
+            // TODO: 24/01/21 handler saving image to firebase
+            // TODO: 24/01/21 initiate BE call to save object
+        });
     }
 
     private void setupCategories(View view) {
@@ -92,10 +107,7 @@ public class CreateRecipeFragment extends Fragment implements RemoveItemClickLis
         AutoCompleteTextView recipeCategoryTextView = view.findViewById(R.id.recipe_category);
         recipeCategoryTextView.setAdapter(categoryAdapter);
 
-        recipeCategoryTextView.setOnItemClickListener((parent, view1, position, id) -> {
-            // TODO: 12/01/21 replace this with actual implementation
-            Toast.makeText(requireContext(), "Category selected: " + categories.get(position), Toast.LENGTH_SHORT).show();
-        });
+        recipeCategoryTextView.setOnItemClickListener((parent, view1, position, id) -> selectedCategory = categories.get(position));
     }
 
     private void setupDuration(View view) {
@@ -107,10 +119,7 @@ public class CreateRecipeFragment extends Fragment implements RemoveItemClickLis
         AutoCompleteTextView durationsTextView = view.findViewById(R.id.time_type);
         durationsTextView.setAdapter(durationsAdapter);
 
-        // TODO: 12/01/21 replace with actual implementation
-        durationsTextView.setOnItemClickListener(
-                (parent, view1, position, id) -> Toast.makeText(requireContext(), "Duration selected: " + durations.get(position), Toast.LENGTH_SHORT)
-                        .show());
+        durationsTextView.setOnItemClickListener((parent, view1, position, id) -> selectedDuration = durations.get(position));
     }
 
     private void setupAddIngredientsChip(View view) {
@@ -215,5 +224,73 @@ public class CreateRecipeFragment extends Fragment implements RemoveItemClickLis
     public void onRemoveClick(String item, int position) {
         preparationSteps.remove(item);
         stepsAdapter.submitList(new ArrayList<>(preparationSteps));
+    }
+
+    private boolean validateForm() {
+        recipe = new Recipe();
+        String name = ((EditText) getView().findViewById(R.id.recipe_name_editText)).getText().toString();
+        if (TextUtils.isEmpty(name)) {
+            showErrorDialog(R.string.error_empty_recipe_name);
+            return false;
+        }
+        recipe.setName(name);
+
+        try {
+            int servingSize = Integer.parseInt(((EditText) getView().findViewById(R.id.serving_size_editText)).getText().toString());
+            recipe.setServingSize(servingSize);
+        } catch (Exception e) {
+            showErrorDialog(R.string.error_empty_recipe_serving_size);
+            return false;
+        }
+
+        if (TextUtils.isEmpty(selectedCategory)) {
+            showErrorDialog(R.string.error_empty_recipe_category);
+            return false;
+        }
+        recipe.setCategory(selectedCategory);
+
+        try {
+            int time = Integer.parseInt(((EditText) getView().findViewById(R.id.recipe_time_editText)).getText().toString());
+            if (TextUtils.isEmpty(selectedDuration)) {
+                showErrorDialog(R.string.error_empty_recipe_duration);
+                return false;
+            }
+
+            recipe.setDuration(selectedDuration.equals("hrs") ? time * 60 : time);
+        } catch (Exception e) {
+            showErrorDialog(R.string.error_empty_recipe_time);
+            return false;
+        }
+
+        try {
+            int calories = Integer.parseInt(((EditText) getView().findViewById(R.id.calorie_editText)).getText().toString());
+            recipe.setCalories(calories);
+        } catch (Exception e) {
+            showErrorDialog(R.string.error_empty_recipe_calories);
+            return false;
+        }
+
+        if (ingredients.isEmpty()) {
+            showErrorDialog(R.string.error_empty_recipe_ingredients);
+            return false;
+        }
+
+        recipe.setIngredients(ingredients);
+
+        if (preparationSteps.isEmpty()) {
+            showErrorDialog(R.string.error_empty_recipe_preparation_steps);
+            return false;
+        }
+        recipe.setPreparationSteps(preparationSteps);
+
+        return true;
+    }
+
+    private void showErrorDialog(@StringRes int msgRes) {
+        new AlertDialog.Builder(requireContext())
+                .setTitle(R.string.error)
+                .setMessage(msgRes)
+                .setPositiveButton(android.R.string.ok, null)
+                .show();
     }
 }
--- app/src/main/java/com/cegep/epicure/model/Recipe.java
+++ app/src/main/java/com/cegep/epicure/model/Recipe.java
@@ -19,4 +19,68 @@ public class Recipe {
     private List<String> ingredients;
 
     private List<String> preparationSteps;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getServingSize() {
+        return servingSize;
+    }
+
+    public void setServingSize(int servingSize) {
+        this.servingSize = servingSize;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getPreparationSteps() {
+        return preparationSteps;
+    }
+
+    public void setPreparationSteps(List<String> preparationSteps) {
+        this.preparationSteps = preparationSteps;
+    }
 }
